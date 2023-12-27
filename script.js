46c8c3ede9731a01abb973533e99ab24ebc69492
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2023-12-27 14:12:05 +0100

    Set up hover effect on grid elements
    When user hover over "pixel" in the grid, it changes color to black

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-sketchArea = document.querySelector('.sketch-area');
+const sketchArea = document.querySelector('.sketch-area');
 
 for (let i = 0; i < 256; i++) {
   let sketchPixel = document.createElement('div');
@@ -6,3 +6,9 @@ for (let i = 0; i < 256; i++) {
   sketchArea.appendChild(sketchPixel);
 }
 
+const allSketchPixels = document.querySelectorAll('.sketch-pixel');
+allSketchPixels.forEach((sketchPixel) => {
+  sketchPixel.addEventListener('mouseenter', () => {
+    sketchPixel.classList.add('coloured');
+  });
+});
