fa2c6cb8b8449a4694f8fffd064c8e1c9c2d3d8e
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2023-12-28 09:41:04 +0100

    Add slider to change grid density

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sketchArea = document.querySelector('.sketch-area');
+const sliderArea = document.querySelector('.slider-area');
+const slider = document.querySelector('.slider');
 
 for (let i = 0; i < 256; i++) {
   let sketchPixel = document.createElement('div');
@@ -7,8 +9,19 @@ for (let i = 0; i < 256; i++) {
 }
 
 const allSketchPixels = document.querySelectorAll('.sketch-pixel');
+const resetButton = document.querySelector('#reset');
+
 allSketchPixels.forEach((sketchPixel) => {
   sketchPixel.addEventListener('mouseenter', () => {
-    sketchPixel.classList.add('coloured');
+    sketchPixel.classList.add('colored');
   });
+  resetButton.addEventListener('click', () => {
+    sketchPixel.classList.remove('colored');
+  });
+});
+
+const sliderValue = document.querySelector('.slider-value');
+slider.addEventListener('input', () => { 
+  sliderValue.textContent = `${slider.value} x ${slider.value}`;
+  sliderArea.appendChild(sliderValue);
 });
