dca603e35d054610a11d62b1d078de23a2ce1946
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2023-12-28 11:13:57 +0100

    Add functionality to grid density slider

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-const sketchArea = document.querySelector('.sketch-area');
 const sliderArea = document.querySelector('.slider-area');
 const slider = document.querySelector('.slider');
-
-for (let i = 0; i < 256; i++) {
-  let sketchPixel = document.createElement('div');
-  sketchPixel.classList.add('sketch-pixel');
-  sketchArea.appendChild(sketchPixel);
-}
-
-const allSketchPixels = document.querySelectorAll('.sketch-pixel');
+const sliderValue = document.querySelector('.slider-value');
+const sketchArea = document.querySelector('.sketch-area');
 const resetButton = document.querySelector('#reset');
 
-allSketchPixels.forEach((sketchPixel) => {
-  sketchPixel.addEventListener('mouseenter', () => {
-    sketchPixel.classList.add('colored');
-  });
-  resetButton.addEventListener('click', () => {
-    sketchPixel.classList.remove('colored');
-  });
-});
+makePixels();
+colorPixels();
 
-const sliderValue = document.querySelector('.slider-value');
 slider.addEventListener('input', () => { 
   sliderValue.textContent = `${slider.value} x ${slider.value}`;
   sliderArea.appendChild(sliderValue);
+  removePixels();
+  makePixels();
+  colorPixels();
 });
+
+function makePixels() {
+  for (let i = 0; i < slider.value * slider.value; i++) {
+    let sketchPixel = document.createElement('div');
+    sketchPixel.classList.add('sketch-pixel');
+    sketchPixel.style.width = 512 / slider.value + 'px';
+    sketchPixel.style.height = 512 / slider.value + 'px';
+    sketchArea.appendChild(sketchPixel);
+  }
+}
+
+function removePixels() {
+  if(sketchArea.childNodes) {
+    allSketchPixels = document.querySelectorAll('.sketch-pixel');
+    allSketchPixels.forEach((sketchPixel) => {
+      sketchPixel.remove();
+    });
+  }
+}
+
+function colorPixels() {
+  let allSketchPixels = document.querySelectorAll('.sketch-pixel');
+  allSketchPixels.forEach((sketchPixel) => {
+    sketchPixel.addEventListener('mouseenter', () => {
+      sketchPixel.classList.add('colored');
+    });
+    resetButton.addEventListener('click', () => {
+      sketchPixel.classList.remove('colored');
+    });
+  });
+}
